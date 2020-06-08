0abb24289bd8b16025e7fce91970f2cee918b466
Fabiany de Sousa <fabianydesousa@gmail.com>
2020-06-08 01:50:58 -0300

    creating functions of the question 1,2,3

--- manipulacao-api-node/src/app.js
+++ manipulacao-api-node/src/app.js
@@ -43,20 +43,81 @@ function countCityInState(uf) {
 }
 // console.log(countCityInState('AC'));
 
-// ========================= QUESTION 3 =========================
-function fiveMostCitiesStates() {
-  let fiveMost = [];
+// ====================== QUESTION 3 / 4 ========================
+let fiveCities = [];
+
+function countFiveCitiesStates() {
   states_cities.forEach((UF) => {
     const sigla = UF.split('.')[0];
-    fiveMost.push({ uf: sigla, length: countCityInState(sigla) });
+    fiveCities.push({ uf: sigla, length: countCityInState(sigla) });
   });
-  fiveMost.sort((a, b) => b.length - a.length);
-  return fiveMost.slice(0, 5);
+  return fiveCities;
 }
-console.log(fiveMostCitiesStates());
+// console.log(countFiveCitiesStates());
+
+// ========================= QUESTION 3 =========================
+function fiveMostCitiesStates() {
+  fiveCities.sort((a, b) => b.length - a.length);
+  return fiveCities.slice(0, 5);
+}
+// console.log(fiveMostCitiesStates());
 
 // ========================= QUESTION 4 =========================
-// ========================= QUESTION 5 =========================
+function fiveSmallerCitiesStates() {
+  fiveCities.sort((a, b) => b.length - a.length);
+  return fiveCities.slice(fiveCities.length - 5, fiveCities.length);
+}
+// console.log(fiveSmallerCitiesStates());
+
+// ======================= QUESTION 5 / 6 =======================
+function lengthNameCitiesState(UF) {
+  let lengthNamesCities = [];
+  const sigla = UF.split('.')[0];
+  const state = JSON.parse(
+    fs.readFileSync(`src/models/states_cities/${UF}`, 'utf8')
+  );
+  state.Cidades.forEach((city) => {
+    lengthNamesCities.push({
+      uf: sigla,
+      name: city.Nome,
+      length: city.Nome.length,
+    });
+  });
+  return lengthNamesCities;
+}
+
+function mostCityNameAllStates() {
+  let mostNameCityStates = [];
+  states_cities.forEach((UF) => {
+    const lengthNames = lengthNameCitiesState(UF);
+    lengthNames.sort((a, b) => b.length - a.length);
+    mostNameCityStates.push(lengthNames[0]);
+  });
+  return mostNameCityStates.sort((a, b) => b.length - a.length);
+}
+// console.log(mostCityNameAllStates());
 // ========================= QUESTION 6 =========================
+function smallestCityNameAllStates() {
+  let smallestNameCityStates = [];
+  states_cities.forEach((UF) => {
+    const lengthNames = lengthNameCitiesState(UF);
+    lengthNames.sort((a, b) => a.length - b.length);
+    smallestNameCityStates.push(lengthNames[0]);
+  });
+  return smallestNameCityStates.sort((a, b) => a.length - b.length);
+}
+// console.log(smallestCityNameAllStates());
+
 // ========================= QUESTION 7 =========================
+function biggestNameCity() {
+  let biggestNameCity = mostCityNameAllStates();
+  return biggestNameCity[0];
+}
+console.log(biggestNameCity());
+
 // ========================= QUESTION 8 =========================
+function smallestNameCity() {
+  let smallestNameCity = smallestCityNameAllStates();
+  return smallestNameCity[0];
+}
+console.log(smallestNameCity());
